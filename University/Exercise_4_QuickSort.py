e20ee295e58452581a7e0a45a8db24ce279d541e
Tien <tienbk1995@gmail.com>
2026-09-06 16:52:21 +0200

    push additional quick_sort analysis

--- University/Exercise_4_QuickSort.py
+++ University/Exercise_4_QuickSort.py
@@ -1,8 +1,14 @@
 # SOLUTION TO EXERCISE 4
-import time, random
+import time, random, math
 
 def _swap(arr, i, j):
     arr[i], arr[j] = arr[j], arr[i]
+
+def pickRandomPivot(low, high):
+    mid = (low + high) // 2
+    x = random.randrange(low, mid)
+    y = random.randrange(mid + 1, high)
+    return x, y
 
 def quick_sort(arr, low, high):
     _quick_sort(arr, low, high)
@@ -50,15 +56,27 @@ def generate_random_list(start, times):
 
 def tabulate_T_n(start, times):
     arr, powerOfTwo = generate_random_list(start, times)
+    low, high = 0, len(arr)-1
+    low, high = pickRandomPivot(0, len(arr)-1)
     start_measure = time.time()
-    quick_sort(arr, 0, len(arr)-1)
+    quick_sort(arr, low, high)
     end_measure = time.time()
     total_time = end_measure - start_measure
     return [(start*powerOfTwo, total_time)]
 
+def calculation(range_of_N, total_time):
+    extended_table = [(range_of_N, total_time ,round(total_time / (math.log2(range_of_N)), 8), round(total_time / (range_of_N * math.log2(range_of_N)), 8), round(total_time / (range_of_N ** 2), 12))]
+    return extended_table
+
+
 if __name__ == '__main__':
     final_results = []
+    extended_table = []
     for i in range(10):
         result = tabulate_T_n(800, i)
+        [(range_of_N, total_time)] = result
         final_results.extend(result)
-        print(result)
+        # print(result)
+
+        extended_table = calculation(range_of_N, total_time)
+        print(extended_table)
